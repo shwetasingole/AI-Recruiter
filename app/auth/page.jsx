c504f8fbf3ed2d2bd2f973fc36a958303a26cfef
shwetasingole <115386998+shwetasingole@users.noreply.github.com>
2025-07-04 05:27:34 +0530

    base url changes

--- app/auth/page.jsx
+++ app/auth/page.jsx
@@ -19,7 +19,7 @@ const Login = () => {
     <div className="flex flex-col items-center justify-center h-screen">
       <div>
         <Image
-          src="/logoipsum-custom-logo.svg"
+          src="/logo-new1.svg"
           alt="logo"
           width={180}
           height={100}
@@ -32,7 +32,7 @@ const Login = () => {
           width={350}
           height={400}
         />
-        <h2 className="text-2xl font-bold">Welcome to AiCruiter</h2>
+        <h2 className="text-2xl font-bold">Welcome to EchoHire</h2>
         <p className="text-gray-500">Sign in with Google Auth</p>
         <Button className="" onClick={signInWithGoogle}>
           Google Sign in
